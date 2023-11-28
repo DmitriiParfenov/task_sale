d5173cce30af1b878a514052cf23f4d4c3d9308e
Dmitrii <dima12sik@yandex.ru>
2023-11-28 21:04:55 +0300

    registered Contact model in the django admin panel

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from contacts.models import Contact
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'country', 'city', 'street', 'number')
+    list_display_links = ('id',)
+    search_fields = ('country', 'city')
+    list_filter = ('country', 'city')
