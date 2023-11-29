7dd9b98ba6b004072069fe0ce463046e4dae9eaf
Dmitrii <dima12sik@yandex.ru>
2023-11-29 21:46:57 +0300

    added Product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Название')
+    model = models.CharField(max_length=150, verbose_name='Модель')
+    release = models.DateField(db_index=True, verbose_name='Дата выхода')
+
+    def __str__(self):
+        return f'Продукт {self.title}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
