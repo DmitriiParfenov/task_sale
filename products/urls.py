07652730769f2ded88912447b7ce0e11d75f3070
Dmitrii <dima12sik@yandex.ru>
2023-11-29 21:56:02 +0300

    added paths to the ProductViewSet

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+
+from products.apps import ProductsConfig
+from products.views import ProductViewSet
+
+app_name = ProductsConfig.name
+router = DefaultRouter()
+router.register('', ProductViewSet, basename='products')
+
+urlpatterns = router.urls
