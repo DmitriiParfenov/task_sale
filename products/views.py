4b593f49c00eba77e798352653bf4b29f0ec1d03
Dmitrii <dima12sik@yandex.ru>
2023-11-29 21:55:17 +0300

    added ProductViewSet with all CRUD methods

--- products/views.py
+++ products/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from contacts.models import Contact
+from contacts.permissions import IsActiveAndIsOwner
+from products.serializers import ProductSerializer, ProductListSerializer
+
 
 # Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    """Для создания, удаления, изменения и получения объектов модели Product."""
+
+    permission_classes = (IsActiveAndIsOwner,)
+    queryset = Contact.objects.all()
+    default_serializer = ProductSerializer
+    serializers = {
+        'list': ProductListSerializer,
+        'retrieve': ProductSerializer,
+    }
+
+    def perform_create(self, serializer):
+        """При создании объекта модели Product текущему объекту присваивается текущий пользователь."""
+
+        new_mat = serializer.save()
+        if not self.request.user.is_staff:
+            new_mat.product_user = self.request.user
+        new_mat.save()
+
+    def get_serializer_class(self):
+        return self.serializers.get(self.action, self.default_serializer)
