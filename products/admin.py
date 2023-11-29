51a6331cfedb20089f3719b59227e169edc945cc
Dmitrii <dima12sik@yandex.ru>
2023-11-29 21:48:06 +0300

    registered Product model in the django admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from products.models import Product
+
+
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'model', 'release')
+    list_display_links = ('id',)
+    search_fields = ('title', 'release')
+    list_filter = ('title', 'release')
