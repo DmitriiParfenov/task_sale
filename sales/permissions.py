d3b485ebd5862126efc7a848cd6ae8dc9e341b1f
Dmitrii <dima12sik@yandex.ru>
2023-12-04 00:53:50 +0300

    added IsActiveAndIsOwner permission

---
+++ sales/permissions.py
@@ -0,0 +1,33 @@
+from rest_framework import permissions
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsActiveAndIsOwner(BasePermission):
+    """
+    Доступ разрешен только активированным пользователям. Детали записей могут смотреть только владельцы записей.
+    Пользователь может видеть список всех объектов без возможности их как-то редактировать или удалять, если он не
+    является их владельцем.
+    """
+    def has_permission(self, request, view):
+        if request.user.is_active:
+            return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            if obj.product.product_user != obj.sale_user:
+                return False
+            if obj.contact.contact_user != obj.sale_user:
+                return False
+            return request.user == obj.sale_user
+        elif request.method in ('PATCH', 'PUT', 'POST'):
+            if obj.product.product_user != obj.sale_user:
+                return False
+            if obj.contact.contact_user != obj.sale_user:
+                return False
+            return request.user == obj.sale_user
+        elif request.method == 'DELETE':
+            if not request.user.is_active:
+                return False
+            return request.user == obj.sale_user or request.user.is_superuser
+        return False
