13a8bb91d964b0b9f47e99d40ddbdfd602cd692b
Dmitrii <dima12sik@yandex.ru>
2023-12-04 00:50:42 +0300

    registered field product_user in the django admin panel

--- products/admin.py
+++ products/admin.py
@@ -6,7 +6,7 @@ from products.models import Product
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'model', 'release')
+    list_display = ('id', 'title', 'model', 'release', 'product_user')
     list_display_links = ('id',)
     search_fields = ('title', 'release')
     list_filter = ('title', 'release')
